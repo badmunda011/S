448222befbc97fd46a982e3abc5796d1f4f6424e
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-08-01 17:26:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ from os import getenv
 
 
 API_ID = 25742938
-API_HASH = getenv("API_HASH", "")
-BOT_USERNAME = getenv("BOT_USERNAME", "YumikooBot")
+API_HASH = "b35b715fe8dc0a58e8048988286fc5b6"
+BOT_USERNAME = "Gaana_MusicBot"
 COMMAND_HANDLER = ["/", "!"]
-BOT_TOKEN = getenv("BOT_TOKEN", "")
+BOT_TOKEN = "7200244120:AAH_Y-UwUOEqyyxMiPe2b104wUVaAIywYn4"
 OWNER_ID = int(getenv("OWNER_ID", "6691393517"))
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6691393517").split()))
-MONGO_URL = getenv("MONGO_URL", "")
-SESSION_STRING = getenv("SESSION_STRING", "")
+MONGO_URL = "mongodb+srv://BADMUNDA:BADMYDAD@badhacker.i5nw9na.mongodb.net/"
+SESSION_STRING = "BQGIzloAfcxANJrgKzZPAYhP5memCBvbKhdV0DvNBp4bnMykBy5M-FIl1mOuPtXxjhgI9cEGQssFzspFMqL-QVQFxFU-l-fJHvNy_IJgLDQYrbPC5bqJwPvcvqBbXim7v9T8s1frMpYcVGDctJYA_KRTrhlc5j1eVZv3nWKlBmOi5ii2r7qw4p856c-2rSOf4b_b_eGB3NL0qCwWgaz83kCd2L6nTCZ2YQisRf0rsjn_9E-CDUYl4a8Y0QpyzQkr8zOLu-pK9a8i4N-pUxT2fTPeLZ_UEXxPR77__LBMIJ2-ZGlzeEkGiJSOBeCWZ6G8PmbO1jaYGiWKxbpyauSIc1FQSEcreQAAAAG7QxP_AA"
